022ad4a53ccddebf3d72a88fed0cba546bd47968
Sergey Nekrasoff <Judixel2015@ya.ru>
2018-01-16 11:31:45 +0400

    creating

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="header">
+    <div class="header__logo">
+      <a href="#">
+        <img src="#" alt="">
+      </a>
+    </div>
+
+    <div class="header__signin">
+      <button class="btn btn-signin" type="button">Sign In</button>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "header",
+  data: () => ({
+
+  })
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
